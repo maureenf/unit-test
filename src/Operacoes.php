60d611395b74842f53f8f7c8a5fc0856f15fca42
Maureen Fernandes <maureen.fernandes@locaweb.com.br>
2019-06-04 15:06:53 -0300

    Added unit test

--- src/Operacoes.php
+++ src/Operacoes.php
@@ -5,31 +5,31 @@ namespace App;
 class Operacoes
 {
 
-	public function soma($a, $b)
-	{
-		return $a + $b;
-	}
+    public function soma($a, $b)
+    {
+        return $a + $b;
+    }
 
-	public function subtracao($a, $b)
-	{
-		return $a - $b;
-	}
+    public function subtracao($a, $b)
+    {
+        return $a - $b;
+    }
 
-	public function divisao($a, $b)
-	{
-		return $a / $b;
-	}
+    public function divisao($a, $b)
+    {
+        return $a / $b;
+    }
 
-	public function multiplicacao($a, $b)
-	{
-		return $a * $b;
-	}
+    public function multiplicacao($a, $b)
+    {
+        return $a * $b;
+    }
 
-	public function mistura($a, $b)
-	{
+    public function mistura($a, $b)
+    {
 
-		$resultadoSoma = $this->soma($a, $b);
+        $resultadoSoma = $this->soma($a, $b);
 
-		return $this->multiplicacao($resultado, $a);
-	}
+        return $this->multiplicacao($resultadoSoma, $a);
+    }
 }
--- tests/OperacoesTest.php
+++ tests/OperacoesTest.php
@@ -8,13 +8,43 @@ use App\Operacoes;
 class OperacoesTest extends TestCase
 {
 
-	public function testSoma()
-	{
-		$operacoes = new Operacoes();
+    public function testSoma()
+    {
+        $operacoes = new Operacoes();
 
-		$result = $operacoes->soma(2, 5);
+        $result = $operacoes->soma(2, 5);
 
-		$this->assertEquals(7, $result);
-	}
+        $this->assertEquals(7, $result);
+    }
+
+    public function testMistura()
+    {
+        $expectedValueA = 4;
+        $expectedValueB = 5;
+        $expectedSomaResult = 9;
+        $expectedMultiplicacaoResult = 36;
+        $operacoesMock = $this->mockOperacoes(['soma', 'multiplicacao']);
+
+        $operacoesMock->expects($this->once())
+            ->method('soma')
+            ->with($expectedValueA, $expectedValueB)
+            ->willReturn($expectedSomaResult);
+
+        $operacoesMock->expects($this->once())
+            ->method('multiplicacao')
+            ->with($expectedSomaResult, $expectedValueA)
+            ->willReturn($expectedMultiplicacaoResult);
+
+        $result = $operacoesMock->mistura($expectedValueA, $expectedValueB);
+
+        $this->assertEquals($expectedMultiplicacaoResult, $result);
+    }
+
+    private function mockOperacoes(array $methods)
+    {
+        return $this->getMockBuilder(Operacoes::class)
+            ->setMethods($methods)
+            ->getMock();
+    }
 
 }
